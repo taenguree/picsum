62165ffee2a1ce6ade0bb9076de0f396eb7983f5
taenguree <mibeeya@gmail.com>
2021-08-09 14:25:19 +0900

    Update: code style

--- app/src/main/java/com/knowre/android/codilitytest/entity/ImageEntity.kt
+++ app/src/main/java/com/knowre/android/codilitytest/entity/ImageEntity.kt
@@ -4,13 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 
 internal data class ImageEntity(
-    val id: Int,
-    val author: String,
-    val width: Int,
-    val height: Int,
-    val url: String,
-    @SerializedName("download_url")
-    val downloadUrl: String
+                                          val id: Int,
+                                          val author: String,
+                                          val width: Int,
+                                          val height: Int,
+                                          val url: String,
+    @SerializedName("download_url") val downloadUrl: String
 
 ) {
 
